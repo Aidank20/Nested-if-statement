a0ae69307d6c98ce41edb0f3704333ea6859ae78
Aidank20 <aidan.k@westcoast_schools.com>
2025-02-09 08:42:15 +0400

    first commit

---
+++ Main.java
@@ -0,0 +1,17 @@
+class main{
+    public static void main(String[] args) {
+        boolean sunny=true;
+        boolean hot=true;
+
+        if(sunny){
+            if(hot){
+                System.out.println("wear cap and short sleeves");    
+            }else{
+                System.out.println("wear short sleeves");
+            }
+        }else{
+            System.out.println("wear jacket and carry an umbrella");
+
+        }
+    }
+}
